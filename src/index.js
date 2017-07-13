40399370dfd181ae5974116e93c9bbb79466773f
ncphillips <ncphillips.19@gmail.com>
2017-07-13 12:19:48 -0300

    feat(createObservableThunk): copied over from forestry app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
+import { Observable } from 'rxjs/Observable'
+
+/**
+ * A H.O.F. that wraps up an Observable returning Function inside a Thunk.
+ * 
+ * @param options 
+ */
 export function createObservableThunk(options) {
-  return function thunk() {
-    return (dispatch, getState) => {
-      // Todo
+  return function thunkCreator(input) {
+    let { method, before, success, failure } = options
+
+    let beforeActions  = ensureArray(before)
+    let successActions = ensureArray(success)
+    let failureActions = ensureArray(failure)
+    
+return function thunk(dispatch, getState) {
+    dispatchMany(beforeActions, input)
+
+    let o = method(input)
+      .do(response => dispatchMany(successActions, response))
+      .catch(error => dispatchMany(failureActions, error) || Observable.of({ error }))
+      .share()
+
+    o.subscribe()
+
+      return o
+
+      function dispatchMany(actionCreators, input) {
+        let date = new Date()
+        actionCreators.forEach((callback) => {
+          let action = callback(input, date)
+          if (action){
+            dispatch(action)
+          } else {
+            console.error(`Invalid Action Creator ${callback.name} returned undefined`)
+          }
+        })
+      }
     }
   }
+}
+
+function ensureArray(callbacks) {
+  if (Array.isArray(callbacks)) {
+    return callbacks
+  }
+
+  if (callbacks) {
+    return [callbacks]
+  }
+
+  return []
 }
